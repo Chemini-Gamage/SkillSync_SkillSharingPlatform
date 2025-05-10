d5c89fc2fd729faf713d7d479f8ccf1ccaeaf324
PrasadiMallawaarachchi <it22088178@my.sliit.lk>
2025-05-10 19:44:02 +0530

    feat: front loginPage

---
+++ Frontend/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import AuthLayout from "./components/AuthLayout"
+import LoginForm from "./components/LoginForm"
+
+const LoginPage = () => {
+  return (
+    <AuthLayout title="Sign in" footerText="New to SkillSync?" footerLink="/signup" footerLinkText="Join now">
+      <LoginForm />
+    </AuthLayout>
+  )
+}
+
+export default LoginPage
