d43d89e8c00f7d4283c5ddf0fa7eb5e732b2f6a2
PrasadiMallawaarachchi <it22088178@my.sliit.lk>
2025-05-10 22:17:02 +0530

    fix: front signupPage

--- Frontend/src/pages/SignupPage.js
+++ Frontend/src/pages/SignupPage.js
@@ -1,5 +1,5 @@
-import AuthLayout from "./components/AuthLayout"
-import SignupForm from "./components/SignupForm"
+import AuthLayout from "../components/AuthLayout"
+import SignupForm from "../components/SignupForm"
 
 const SignupPage = () => {
   return (
